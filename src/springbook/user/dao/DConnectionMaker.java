310f737ffd683c6a8cede868799e0d87987a47ed
NakOh <kk0704@naver.com>
2014-09-27 01:28:33 +0900

    Interface
    인터페이스도�

--- src/springbook/user/dao/DConnectionMaker.java
+++ src/springbook/user/dao/DConnectionMaker.java
@@ -4,10 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-	public Connection makeNewConnection() throws ClassNotFoundException, SQLException{
+public class DConnectionMaker implements ConnectionMaker{
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException,
+			SQLException {
+		// TODO Auto-generated method stub
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "root", "autoset");
 		return c;
 	}
+
 }
