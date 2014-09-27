252d95be669c15d4304446e163c38c4356922439
NakOh <kk0704@naver.com>
2014-09-28 00:02:39 +0900

    XML도�
    띄어쓰기 씨...

--- src/springbook/user/dao/Main.java
+++ src/springbook/user/dao/Main.java
@@ -2,7 +2,7 @@ package springbook.user.dao;
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import springbook.user.domain.User;
 
@@ -10,7 +10,7 @@ import springbook.user.domain.User;
 public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("springbook/user/dao/applicationContext.xml");
 		UserDao dao= context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
